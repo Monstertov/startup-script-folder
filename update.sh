59d7156565ff09345269d44ddd9a6649d247cab1
Monstertov <admin@tov.monster>
2021-01-09 17:33:23 +0100

    remove redundant use of basename

--- update.sh
+++ update.sh
@@ -7,6 +7,6 @@ do
 		f=$(basename $f)
 		sudo cp $f /etc/init.d/$f # Copy the file to init.d
 		sudo chmod +x /etc/init.d/ # Make sure file is executable
-		sudo update-rc.d $(basename $f) defaults # Updates the System V style init script links for each file 
+		sudo update-rc.d $f defaults # Updates the System V style init script links for each file 
 	fi
 done
